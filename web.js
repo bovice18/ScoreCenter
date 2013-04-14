3ba44d1fcab587f087346339666129c11b076bca
bovice18 <beau.wood@tufts.edu>
2013-04-14 18:54:56 -0400

    Should send the JSON from the mongo db

--- web.js
+++ web.js
@@ -27,10 +27,8 @@ app.get('/', function(request, response) {
 	response.set('Content-Type', 'text/html');
 	db.High_Scores.find({}).limit(10).sort({game_title:1}, function(err, scores){
 		if(err || !High_Scores) console.log("no scores");
-		else High_Scores.forEach( function(score){
-			scoreList += JSON.stringify(score) + ",";
+		else response.send(High_Scores);	
 			} );
-		response.send(scoreList);
 	} );
 });
 
